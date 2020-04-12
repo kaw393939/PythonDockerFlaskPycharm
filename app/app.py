43d1404a00a898be43dbe042f2383497e844f665
kaw393939 <keith@webizly.com>
2020-04-12 21:25:03 +0200

    Task: Updated Project for lesson2

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -26,12 +27,21 @@ def cities_import() -> List[Dict]:
     return result
 
 
+
+
+
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities)
+
+@app.route('/api/cities')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `fldLat` NUMERIC(6, 4),
     `fldLong` NUMERIC(7, 4),
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
-    `fldAbreviation` VARCHAR(3) CHARACTER SET utf8,
+    `fldAbbreviation` VARCHAR(3) CHARACTER SET utf8,
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
     `fldPopulation` INT
 );
